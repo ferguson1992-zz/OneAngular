da107044934b43984636ad276b956ddedd147b6e
Ferguson <ferromrob@gmail.com>
2019-02-22 17:29:56 -0600

    Cambios del video 34

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'section3';
+  nombre:string = 'Fernando';
+  apellido:string = 'Roman';
 }
